3fbcd6661a2c06c1221595593d70e48c21b2b95a
Oleg Salionov <oleg@salionov.ru>
2013-02-27 23:32:03 +1100

    minor

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ exports.workplaces = function(req, res) {
             {text: "My workplaces"},
         ],
         jobs: [
-            {name: "VVSU", descr: "MSSQL, PHP, JS, VB, C#", time: "2008-2012"}, 
+            {name: "VVSU", descr: "PHP, JS, VB, C#, MSSQL", time: "2008-2012"}, 
             {name: "KamaGames", descr: "JAVA, PostgreSQL", time: "2012-2013"},
             {name: "Drom.ru", descr: "PHP, NodeJS, MySQL, Mongo", time: "2013-..."}
         ]
